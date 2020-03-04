bfbd4d0e954f2318ae7c62b18e32d28387d355e6
Oryan Moshe <oryan@moshe.ai>
2020-03-04 19:42:33 +0200

    Finished action logic and documentation

---
+++ README.md
@@ -0,0 +1,62 @@
+# AWS ECS `servicesStable` waiter (with retries)
+
+AWS provides a way to wait for certain ECS services to become `stable`, but this command times out after 10 minutes.\
+This action allows you to wait for services to become stable **and** retry the waiting process as many times as you want.
+
+## Inputs
+
+### `aws-access-key-id`
+
+**Required** - _string_\
+Your `AWS_ACCESS_KEY_ID`.
+
+### `aws-secret-access-key` - ``
+
+**Required** - _string_\
+Your `AWS_SECRET_ACCESS_KEY`.
+
+### `aws-region`
+
+**Required** - _string_\
+Your `AWS_REGION`.
+
+### `ecs-cluster`
+
+**Required** - _string_\
+The ECS cluster that contains your services.
+
+### `ecs-services`
+
+**Required** - _string[]_\
+A list of ECS services to make sure are stable.
+
+### `retries`
+
+**Required** - _integer_\
+The number of times you want to try the stability check. Default `2`.
+
+### `verbose`
+
+_Optional_ - _boolean_\
+Whether to print verbose debug messages to the console. Default `false`.
+
+## Outputs
+
+### `retries`
+
+_integer_\
+How many retries happened until success.
+
+## Example usage
+
+```yaml
+uses: oryanmoshe/ecs-wait-action@v1
+with:
+  aws-access-key-id: "AKIAIOSFODNN7EXAMPLE"
+  aws-secret-access-key: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  aws-region: "us-east-1"
+  ecs-cluster: "my-ecs-cluster"
+  ecs-services: ["my-ecs-service-1", "my-ecs-service-2"]
+  retries: 5
+  verbose: false
+```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,96 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+const AWS = require("aws-sdk");
 
-try {
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
+/**
+ * Waits for given AWS ECS services transition into "servicesStable" state.
+ * Times out after 10 minutes.
+ * @param   {Object}   params
+ * @param   {AWS.ECS}  params.ecsConnection - An AWS ECS connection object
+ * @param   {string}   params.cluster       - The name of the ECS cluster
+ * @param   {string[]} params.services      - A list of ECS services to check for stability
+ * @returns {Promise}                         A promise to be resolved when services are stable or rejected after the timeout
+ */
+const waitForStability = ({ ecsConnection, cluster, services }) =>
+  ecsConnection.waitFor("servicesStable", { cluster, services }).promise();
 
-  core.setOutput("time", time);
+/**
+ * Retries the ECS services stability check for the given amount of retries.
+ * @param   {Object}   params
+ * @param   {number}   params.retries - The number of times to retry the stability check
+ * @param   {boolean}  params.verbose - Whether to print verbose log messages
+ * @param   {Object}   params.params  - The rest of the parameters
+ * @returns {number}                    The number of tries we did
+ */
+const retry = async ({ retries, verbose, ...params }) => {
+  let currTry = 1;
+  let isStable = false;
+  while (currTry <= retries && !isStable) {
+    try {
+      if (verbose) {
+        console.info(`Waiting for service stability, try #${currTry}`);
+      }
+      await waitForStability(params);
+      isStable = true;
+    } catch {
+      if (verbose) {
+        console.warn(`Try #${currTry} failed!`);
+      }
+      ++currTry;
+    }
+  }
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  return currTry;
+};
 
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+/**
+ * Creates an AWS ECS connection using the given credentials.
+ * @param   {Object}  params
+ * @param   {string}  params.accessKeyId     - The AWS_ACCESS_KEY_ID
+ * @param   {string}  params.secretAccessKey - The AWS_SECRET_ACCESS_KEY
+ * @param   {string}  params.region          - The AWS_REGION
+ * @returns {AWS.ECS}                          An AWS ECS connection object
+ */
+const createEcsConnection = ({ accessKeyId, secretAccessKey, region }) =>
+  new AWS.ECS({
+    apiVersion: "2014-11-13",
+    accessKeyId,
+    secretAccessKey,
+    region
+  });
+
+/**
+ * The GitHub Action entry point.
+ */
+const main = async () => {
+  try {
+    const params = {
+      accessKeyId: core.getInput("aws-access-key-id"),
+      secretAccessKey: core.getInput("aws-secret-access-key"),
+      region: core.getInput("aws-region"),
+      retries: core.getInput("retries"),
+      cluster: core.getInput("ecs-cluster"),
+      services: core.getInput("ecs-services"),
+      verbose: core.getInput("verbose")
+    };
+
+    const ecsConnection = createEcsConnection(params);
+    params["ecsConnection"] = ecsConnection;
+
+    const actualRetries = retry(params);
+    if (actualRetries > params.retries) {
+      if (verbose) {
+        console.error(`Service is not stable after ${params.retries} retries!`);
+      }
+      core.setFailed(`Service is not stable after ${params.retries} retries!`);
+    } else {
+      if (verbose) {
+        console.log(`Service is stable after ${actualRetries} retries!`);
+      }
+      core.setOutput("retries", actualRetries);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
